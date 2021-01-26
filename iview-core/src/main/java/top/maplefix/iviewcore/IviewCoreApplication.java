bbc15471370f5aa16bba7656b61c7d2b43a2c41f
maplefix <maplefix@163.com>
2021-01-26 11:06:55 +0800

    :tada: init: 初始化iview-core

---
+++ iview-core/src/main/java/top/maplefix/iviewcore/IviewCoreApplication.java
@@ -0,0 +1,13 @@
+package top.maplefix.iviewcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IviewCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IviewCoreApplication.class, args);
+    }
+
+}
